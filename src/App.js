072c4ded61176425c839a04794951bc2182f3326
BradTheeStallion <bradley.ayers@keyin.com>
2025-04-15 13:11:50 -0230

    Configured App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import BinarySearchTree from './pages/BinarySearchTree';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <Router>
-          <Routes>
-            <Route path="/" element={<BinarySearchTree />} />
-          </Routes>
-        </Router>
+        <BinarySearchTree />
     </div>
   );
 }
